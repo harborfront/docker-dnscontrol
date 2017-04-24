040bbfddfd2dfc4999c4637ce3656da885c731ec
Lionell Yip <watonly.me@gmail.com>
2017-04-25 00:18:19 +0800

    Set a default if nothing is passed

--- dnscontrol.sh
+++ dnscontrol.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -xe
+
+if [ -z ${PLUGIN_METHOD+x} ]; then
+  /bin/dnscontrol-Linux -creds /bin/creds.json version
+  /bin/dnscontrol-Linux -creds /bin/creds.json preview
+  /bin/dnscontrol-Linux -creds /bin/creds.json push
+fi
 
 # PLUGIN_COMMAND variable for github.com/drone/drone CI
 /bin/dnscontrol-Linux -creds /bin/creds.json $@ ${PLUGIN_METHOD}
