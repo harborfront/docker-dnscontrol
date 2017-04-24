cd66b67cbe8ba33865336930d84c23fcd6b9c42a
Lionell Yip <watonly.me@gmail.com>
2017-04-24 23:54:54 +0800

    Added Drone CI support

--- dnscontrol.sh
+++ dnscontrol.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/bin/dnscontrol-Linux -creds /bin/creds.json $@
+# PLUGIN_COMMAND variable for github.com/drone/drone CI
+/bin/dnscontrol-Linux -creds /bin/creds.json $@ ${PLUGIN_COMMAND}
